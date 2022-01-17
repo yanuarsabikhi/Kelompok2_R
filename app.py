5372069746f55da36b0f97a517197b12fe2bb0c8
Yanuar Sabikhi <97417159+yanuarsabikhi@users.noreply.github.com>
2022-01-17 10:02:54 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+application = Flask(__name__)
+@application.route('/')
+def index():
+    return render_template('index.html')
+
+@application.route('/fibo')
+def fibo():
+    return render_template('fibonacci.html')
+
+@application.route('/faktorial')
+def faktorial():
+    return render_template('faktorial.html')
+
+@application.route('/ganjilgenap')
+def ganjilgenap():
+    return render_template('ganjilgenap.html')
+
+if __name__ == '__main__':
+    application.run(debug=True)
